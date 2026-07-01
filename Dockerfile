cf8e7a4b336ade0b265d96dcfe68ab623be1efa8
Facundo Dualde <facu.dualde@gmail.com>
2026-06-30 23:32:25 -0300

    Fix: run container as host user.

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,12 @@ RUN apt-get update \
     && apt-get install -y --no-install-recommends ffmpeg \
     && rm -rf /var/lib/apt/lists/*
 
+# This is just a default value in case the env
+# variable is absent.
+ARG VIDEO_GID=44
+ARG RENDER_GID=992
+RUN getent group ${VIDEO_GID}  >/dev/null || groupadd -g ${VIDEO_GID}  host_video; \
+    getent group ${RENDER_GID} >/dev/null || groupadd -g ${RENDER_GID} host_render
+
 COPY requirements.txt /tmp/requirements.txt
 RUN pip install --no-cache-dir -r /tmp/requirements.txt
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-# Create video and render grupos if they don't already exist in the container.
-getent group "${VIDEO_GID}" >/dev/null || groupadd -g "${VIDEO_GID}" host_video 2>/dev/null
-getent group "${RENDER_GID}" >/dev/null || groupadd -g "${RENDER_GID}" host_render 2>/dev/null
-
-# Change to host user.
-exec chroot --userspec="${UID}:${GID}" / env \
+exec env \
     HIP_VISIBLE_DEVICES="${HIP_VISIBLE_DEVICES}" \
     HSA_OVERRIDE_GFX_VERSION="${HSA_OVERRIDE_GFX_VERSION}" \
     PYTHONUNBUFFERED="${PYTHONUNBUFFERED}" \
